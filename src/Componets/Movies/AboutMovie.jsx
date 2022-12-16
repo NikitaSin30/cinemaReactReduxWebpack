b83b39fc99b93f63f194059045222f12efc72659
Nikita Sinelnikov <nikita.sinelnikov@lenta.ru>
2022-12-16 22:39:31 +0300

    /

--- src/Componets/Movies/AboutMovie.jsx
+++ src/Componets/Movies/AboutMovie.jsx
@@ -31,9 +31,14 @@ function AboutMovie() {
                            {movie.description}
                         </p>
                         <div className="aboutMvoie-info__trailer">
-                           <video controls="controls">
-                              <source src={movie.trailer} />
-                           </video>
+                           <iframe
+                              width="560"
+                              height="315"
+                              src="https://www.youtube.com/embed/7-W7gzBkNz0"
+                              title="YouTube video player"
+                              frameborder="0"
+                              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                              allowfullscreen></iframe>
                         </div>
                      </div>
                   </div>
